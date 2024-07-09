405f585567563d7980fcafa731e85d5184a3906b
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-07-09 20:26:00 +0330

    Added icons to sidebar

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import {AiOutlineHome} from "react-icons/ai";
+import {MdProductionQuantityLimits} from "react-icons/md";
+import {BiCommentDetail} from "react-icons/bi";
+import {FiUsers} from "react-icons/fi";
+import {BsBagCheck, BsCurrencyDollar} from "react-icons/bs";
 
 export default function SideBar() {
     // JSX
@@ -9,26 +14,41 @@ export default function SideBar() {
 
             <ul className="mt-5 [&>li]:relative [&>li]:p-[15px] [&>li>a]:flex [&>li>a]:items-center [&>li>a]:justify-start [&>li>a]:w-full [&>li>a]:text-[var(--white)] [&>li>a]:text-[1.3rem]">
                 {/* sidebar-active-item is custom class (active item) */}
-                <li className="mb-5">
-                    <a href="#">صفحه اصلی</a>
+                <li className="mb-5 hover:sidebar-active-item">
+                    <a href="#">
+                        <AiOutlineHome className="ml-[8px]"/>
+                        صفحه اصلی
+                    </a>
                 </li>
-                <li className="sidebar-active-item">
-                    <a href="#">محصولات</a>
+                <li className="sidebar-active-item hover:sidebar-active-item hover:sidebar-active-item">
+                    <a href="#">
+                        <MdProductionQuantityLimits className="ml-[8px]"/>
+                        محصولات
+                    </a>
                 </li>
-                <li>
-                    <a href="#">کامنت ها</a>
+                <li className="hover:sidebar-active-item">
+                    <a href="#">
+                        <BiCommentDetail className="ml-[8px]"/>
+                        کامنت ها
+                    </a>
                 </li>
-                <li>
-                    <a href="#">کاربران</a>
+                <li className="hover:sidebar-active-item">
+                    <a href="#">
+                        <FiUsers className="ml-[8px]"/>
+                        کاربران
+                    </a>
                 </li>
-                <li>
-                    <a href="#">صفحه اصلی</a>
+                <li className="hover:sidebar-active-item">
+                    <a href="#">
+                        <BsBagCheck className="ml-[8px]"/>
+                        سفارشات
+                    </a>
                 </li>
-                <li>
-                    <a href="#">سفارشات</a>
-                </li>
-                <li>
-                    <a href="#">تخفیف ها</a>
+                <li className="hover:sidebar-active-item">
+                    <a href="#">
+                        <BsCurrencyDollar className="ml-[8px]"/>
+                        تخفیف ها
+                    </a>
                 </li>
             </ul>
         </div>

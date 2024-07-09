f2157561dbca5b7e1420cb4f4d1f9cdae5b1021b
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-07-10 02:03:47 +0330

    Development header section (styles)

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,28 +5,35 @@ import {BsBrightnessHigh} from "react-icons/bs";
 export default function Header() {
     //JSX
     return (
-        <div className="Header">
-            <div className="admin-profile">
-                <img src="/images/admin-profile01.jpg" alt="admin profile"/>
-                <div>
-                    <h2>محمدرضا بهین کر</h2>
-                    <h3>برنامه نویس</h3>
+        <div className="flex items-center justify-between w-full">
+            <div className="flex items-center gap-5">
+                <img src="/images/admin-profile01.jpg" alt="admin profile" className="w-[50px] rounded-full"/>
+                <div className="flex flex-col">
+                    <h2 className="text-[1.2rem]">محمدرضا بهین کر</h2>
+                    <h3 className="text-[#858585]">برنامه نویس</h3>
                 </div>
             </div>
 
-            <div className="header-left-section">
-                <div className="search-box">
-                    <input type="text" placeholder="جستجو کنید..."/>
-                    <button>جستجو</button>
-
-                    {/* Buttons */}
-                    <button className="header-left-icon">
-                        <AiOutlineBell/>
-                    </button>
-                    <button className="header-left-icon">
-                        <BsBrightnessHigh/>
+            <div
+                className="flex gap-5 [&>button]:flex [&>button]:items-center [&>button]:justify-center [&>button]:size-10 [&>button]:p-[10px] [&>button]:bg-[var(--blue)] [&>button]:text-[var(--white)] [&>button]:rounded-[10px] [&>button]:search-box-shadow">
+                {/* search box */}
+                {/* search-box-shadow is custom class */}
+                <div
+                    className="flex items-center justify-between w-[400px] h-[45px] bg-[var(--white)] pl-[5px] rounded-[15px] overflow-hidden search-box-shadow">
+                    <input type="text" placeholder="جستجو کنید..."
+                           className="w-full text-[1.1rem] py-[10px] pl-[10px] pr-[20px] outline-none focus:outline-none"/>
+                    <button
+                        className="flex w-[100px] bg-[var(--blue)] text-[1.1rem] text-[var(--white)] py-[3px] px-[20px] rounded-[10px]">جستجو
                     </button>
                 </div>
+
+                {/* Buttons */}
+                <button className="header-left-icon">
+                    <AiOutlineBell/>
+                </button>
+                <button className="header-left-icon">
+                    <BsBrightnessHigh/>
+                </button>
             </div>
         </div>
     );

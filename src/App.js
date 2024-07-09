92f5aaf4193006d1c339da55f86ece793255b8b3
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-07-10 00:52:28 +0330

    Development header template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import SideBar from "./Components/SideBar/SideBar";
+import Header from "./Components/Header/Header";
 
 function App() {
     return (
         <div className="App">
             <SideBar/>
+
+            <main className="flex-[4] p-5 pr-[280px]">
+                <Header/>
+
+                {/* Router */}
+            </main>
         </div>
     );
 }

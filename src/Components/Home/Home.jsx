3261a8e3f60323249705ee6948b24b1f53e28076
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-07-11 00:19:10 +0330

    Development modal box in home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 export default function Home() {
     //JSX
     return (
-        <div>
-            home
+        <div className="text-[2rem] text-[var(--white)] text-center bg-green-600 mt-5 p-5">
+            <h2>به پنل مدیریت خود خوش آمدید.</h2>
+            <p className="text-lg">جهت مدیریت بخش مربوطه را از منو انتخاب کنید.</p>
         </div>
     );
 }

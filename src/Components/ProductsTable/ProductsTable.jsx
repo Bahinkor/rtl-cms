9fb26f548c5c6ee515ebcaff26220d2c936f8c2d
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-07-11 23:05:46 +0330

    Development products table (styles)

--- src/Components/ProductsTable/ProductsTable.jsx
+++ src/Components/ProductsTable/ProductsTable.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 export default function ProductsTable() {
     //JSX
     return (
-        <table className="products-table">
+        <table className="w-full bg-[var(--white)] mt-[30px] rounded-[10px]">
             {/* table header */}
             <thead>
-            <tr>
+            <tr className="flex justify-between text-center pr-[70px] pl-[460px]">
                 <th>عکس</th>
                 <th>اسم</th>
                 <th>قیمت</th>
@@ -17,14 +17,15 @@ export default function ProductsTable() {
             {/* products list */}
             <tbody>
 
-            <tr>
+            <tr className="flex justify-between text-center pr-[10px] pl-[30px] [&>td]:flex [&>td]:items-center [&>td]:p-5">
                 <td>
-                    <img src="/images/admin-profile01.jpg" alt="product image"/>
+                    <img src="/images/admin-profile01.jpg" alt="product image"
+                         className="w-[150px] rounded-[10px] object-cover"/>
                 </td>
-                <td>روغن سزخ کردنی</td>
+                <td>روغن سرخ کردنی</td>
                 <td>۹۲,۰۰۰ تومان</td>
                 <td>۸۲</td>
-                <td>
+                <td className="[&>button]:text-[var(--white)] [&>button]:text-[1.1rem] [&>button]:bg-[var(--blue)] [&>button]:py-2 [&>button]:px-5 [&>button]:mr-5 [&>button]:rounded-[10px]">
                     <button>جزئیات</button>
                     <button>حذف</button>
                     <button>ویرایش</button>

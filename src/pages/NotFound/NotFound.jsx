72a26dd62821c8f4edca07ddcf735c408333ee35
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-07-13 00:35:53 +0330

    Development not found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 export default function NotFound() {
     //JSX
     return (
-        <h1>Not found</h1>
+        <div className="text-[2rem] text-[var(--white)] text-center bg-[#ff4f4f] mt-5 p-5">
+            <h2>صفحه مورد نظر یافت نشد.</h2>
+        </div>
     )
 }

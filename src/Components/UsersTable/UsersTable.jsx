8777cba412c92f7befedb409fec4bee420d5bd64
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-07-18 23:12:49 +0330

    Development delete user (api)

--- src/Components/UsersTable/UsersTable.jsx
+++ src/Components/UsersTable/UsersTable.jsx
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import ErrorBox from "../ErrorBox/ErrorBox";
 import Loading from "../Loading/Loading";
+import DeleteModal from "../DeleteModal/DeleteModal";
 import {toast} from "react-toastify";
 
 export default function UsersTable() {
     //state
     const [allUsers, setAllUsers] = useState(null);
+    const [isShowDeleteModal, setIsShowDeleteModal] = useState(null);
+    const [mainUserID, setMainUserID] = useState(null);
 
     //react-toastify package function
     const successNotification = () => toast.success("عملیات موفقیت آمیز بود.", {
@@ -28,6 +31,26 @@ export default function UsersTable() {
             .then(data => setAllUsers(data))
             .catch(err => console.log(err));
 
+    }
+
+    const closeDeleteModal = () => setIsShowDeleteModal(false);
+
+    const deleteModalSubmitAction = () => {
+
+        fetch(`http://localhost:3000/api/users/${mainUserID}`, {
+            method: "DELETE",
+        })
+            .then(res => res.json())
+            .then(data => {
+                successNotification();
+                getAllUsers();
+            })
+            .catch(err => {
+                errorNotification();
+                console.log(err);
+            })
+
+        closeDeleteModal();
     }
 
     //useEffect
@@ -70,7 +93,11 @@ export default function UsersTable() {
                                     <td>{user.email}</td>
                                     <td className="[&>button]:btn !p-[5px]">
                                         <button className="blue-btn">جزئیات</button>
-                                        <button className="red-btn">حذف</button>
+                                        <button className="red-btn" onClick={() => {
+                                            setMainUserID(user.id);
+                                            setIsShowDeleteModal(true)
+                                        }}>حذف
+                                        </button>
                                         <button className="green-btn">ویرایش</button>
                                     </td>
                                 </tr>
@@ -83,6 +110,12 @@ export default function UsersTable() {
                     <ErrorBox message="هیچ کاربری یافت نشد."/>
                 )
 
+            }
+
+            {/* Delete Modal */}
+            {
+                isShowDeleteModal && <DeleteModal cancelAction={closeDeleteModal} submitAction={deleteModalSubmitAction}
+                                                  title="آیا از حذف مطمئن هستید؟"/>
             }
 
         </>

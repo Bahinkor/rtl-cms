a03242f598aab152123479bfa02d29497a383dcf
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-07-18 22:55:25 +0330

    Development users table (logic)

--- src/Components/UsersTable/UsersTable.jsx
+++ src/Components/UsersTable/UsersTable.jsx
@@ -1,43 +1,91 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import ErrorBox from "../ErrorBox/ErrorBox";
+import Loading from "../Loading/Loading";
+import {toast} from "react-toastify";
 
 export default function UsersTable() {
+    //state
+    const [allUsers, setAllUsers] = useState(null);
+
+    //react-toastify package function
+    const successNotification = () => toast.success("عملیات موفقیت آمیز بود.", {
+        rtl: true,
+        pauseOnHover: false,
+        autoClose: 3000,
+    });
+
+    const errorNotification = () => toast.error("اوه، با خطا مواجه شدیم!", {
+        rtl: true,
+        pauseOnHover: false,
+        autoClose: 3000,
+    });
+
+    //function
+    const getAllUsers = async () => {
+
+        await fetch("http://localhost:3000/api/users/")
+            .then(res => res.json())
+            .then(data => setAllUsers(data))
+            .catch(err => console.log(err));
+
+    }
+
+    //useEffect
+    useEffect(() => {
+        getAllUsers();
+    }, [])
+
     //JSX
     return (
         <>
 
-            <table className="w-full bg-[var(--white)] mt-[30px] rounded-[10px]">
-                {/* table header */}
-                <thead>
-                <tr className="flex items-center w-full text-center [&>*]:w-full [&>*]:p-5">
-                    <th>نام و نام خانوادگی</th>
-                    <th>یوزرنیم</th>
-                    <th>پسورد</th>
-                    <th>شماره تماس</th>
-                    <th>ایمیل</th>
-                    <th>اکشن</th>
-                </tr>
-                </thead>
+            {
+                allUsers === null ? (
+                    <Loading/>
+                ) : allUsers.length ? (
+                    < table className="w-full bg-[var(--white)] mt-[30px] rounded-[10px]">
+                        {/* table header */}
+                        <thead>
+                        <tr className="flex items-center w-full text-center [&>*]:w-full [&>*]:p-5">
+                            <th>نام و نام خانوادگی</th>
+                            <th>یوزرنیم</th>
+                            <th>پسورد</th>
+                            <th>شماره تماس</th>
+                            <th>ایمیل</th>
+                            <th>اکشن</th>
+                        </tr>
+                        </thead>
 
-                {/* Users list */}
-                <tbody>
+                        {/* Users list */}
+                        <tbody>
 
-                <tr
-                    className="flex items-center w-full text-center [&>*]:w-full [&>*]:p-5">
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td className="[&>button]:btn !p-[5px]">
-                        <button className="blue-btn">جزئیات</button>
-                        <button className="red-btn">حذف</button>
-                        <button className="green-btn">ویرایش</button>
-                    </td>
-                </tr>
+                        {
+                            allUsers.map(user => (
+                                <tr key={user.id}
+                                    className="flex items-center w-full text-center [&>*]:w-full [&>*]:p-5">
+                                    <td>{user.firsname} {user.lastname}</td>
+                                    <td>{user.username}</td>
+                                    <td>{user.password}</td>
+                                    <td>{user.phone}</td>
+                                    <td>{user.email}</td>
+                                    <td className="[&>button]:btn !p-[5px]">
+                                        <button className="blue-btn">جزئیات</button>
+                                        <button className="red-btn">حذف</button>
+                                        <button className="green-btn">ویرایش</button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
 
-                </tbody>
-            </table>
+                        </tbody>
+                    </table>
+                ) : (
+                    <ErrorBox message="هیچ کاربری یافت نشد."/>
+                )
+
+            }
 
         </>
-    );
+    )
+        ;
 }
--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import UsersTable from "../../Components/UsersTable/UsersTable";
-import ErrorBox from "../../Components/ErrorBox/ErrorBox";
 
 export default function Users() {
     //JSX
     return (
         <div className="mt-[60px] p-5 rounded-xl">
-            <ErrorBox message="هیچ کاربری یافت نشد."/>
             <UsersTable/>
         </div>
     );

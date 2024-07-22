54f3d34223059660389d320218eefc2e4beb359e
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-07-23 00:32:48 +0330

    Development order items section

--- src/Components/OrdersTable/OrdersTable.jsx
+++ src/Components/OrdersTable/OrdersTable.jsx
@@ -1,20 +1,39 @@
 import React, {useState, useEffect} from 'react';
 import Loading from '../Loading/Loading';
 import ErrorBox from '../ErrorBox/ErrorBox';
+import DetailsModal from '../DetailsModal/DetailsModal';
 
 export default function OrdersTable() {
     //state
     const [allOrders, setAllOrders] = useState(null);
+    const [mainOrderItems, setMainOrderItems] = useState([]);
+    const [mainOrderID, setMainOrderID] = useState(null);
+    const [isSowDetailsModal, setIsSowDetailsModal] = useState(false);
 
     //function
     const getAllOrders = async () => {
-        fetch("http://localhost:8000/orders/", {
+        await fetch("http://localhost:8000/orders/", {
             headers: {
                 "Authorization": "Token 502387428aee0698042273c57145ed5aea88cadb",
             }
         })
             .then(res => res.json())
             .then(data => setAllOrders(data))
+            .catch(err => console.log(err));
+    }
+
+    const closeDetailsModal = () => {
+        setIsSowDetailsModal(false);
+    }
+
+    const getMainOrderItems = async () => {
+        await fetch(`http://localhost:8000/orders/detail/items/${mainOrderID}/`, {
+            headers: {
+                "Authorization": "Token 502387428aee0698042273c57145ed5aea88cadb",
+            }
+        })
+            .then(res => res.json())
+            .then(data => setMainOrderItems(data))
             .catch(err => console.log(err));
     }
 
@@ -53,7 +72,12 @@ export default function OrdersTable() {
                                     <td>{order.is_active ? "فعال" : "غیرفعال"}</td>
                                     <td>{order.order_code}</td>
                                     <td className="[&>button]:btn !p-[5px]">
-                                        <button className="green-btn">خالی</button>
+                                        <button className="blue-btn" onClick={() => {
+                                            setMainOrderID(order.order_code);
+                                            setIsSowDetailsModal(true);
+                                            getMainOrderItems();
+                                        }}>جزئیات
+                                        </button>
                                     </td>
                                 </tr>
                             ))
@@ -63,6 +87,35 @@ export default function OrdersTable() {
                     </table>
                 ) : (
                     <ErrorBox message="هیچ سفارشی یافت نشد."/>
+                )
+            }
+
+            {/* Details Modal */}
+            {
+                isSowDetailsModal && (
+                    <DetailsModal onClose={closeDetailsModal}>
+                        <table className="w-full bg-[var(--white)] mt-5 rounded-[10px]">
+
+                            <thead>
+                            <tr className="text-center [&>*]:p-5">
+                                <th>محصول</th>
+                                <th>کد محصول</th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            {
+                                mainOrderItems.map(item => (
+                                    <tr key={item.item_code} className="text-center [&>*]:p-5">
+                                        <td>{item.product.title}</td>
+                                        <td>{item.product.product_code}</td>
+                                    </tr>
+                                ))
+                            }
+                            </tbody>
+
+                        </table>
+                    </DetailsModal>
                 )
             }
         </>
